614eb21cebff300e29a01635d8ef4c4d7fb4befd
chenxuesdu <chenxuesdu@gmail.com>
2016-03-25 23:30:29 -0700

    Update
    Update delete and Update

--- src/main/java/com/cmpe275/controllers/ProfileController.java
+++ src/main/java/com/cmpe275/controllers/ProfileController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cmpe275.exceptions.ResourceNotFoundException;
 import com.cmpe275.model.Profile;
@@ -22,34 +21,48 @@ public class ProfileController {
 	@Autowired
 	ProfileDao profileDao;
 
-	@RequestMapping("/greeting")
-	public String hello(
-			@RequestParam(value = "firstname", required = false, defaultValue = "World") String firstname,
-			Model model) {
-		model.addAttribute("firstname", firstname);
-		return "hello";
-	}
+	// question 5
 	@RequestMapping("/profile/delete/{id}")
-	public String deleteProfile(@PathVariable(value = "id") String id) {
+	public String deleteProfile(@PathVariable(value = "id") String id,
+			Model model) {
 		try {
 			Profile profile = profileDao.findById(id);
 			if (profile == null) {
-				throw new ResourceNotFoundException("Profile not found!");
+				throw new ResourceNotFoundException(
+						"Sorry, the requested user with id" + id
+								+ "does not exits!");
 			}
 			this.profileDao.delete(profile);
-			return "create";
+			model.addAttribute("profile", new Profile());
+			return "redirect:/profile";
 		} catch (Exception e) {
 			throw new ResourceNotFoundException("Profile not found!");
 		}
 	}
-	
+
+	// question 1 updte
+	@RequestMapping(value = "/profile/update")
+	public String UpdateProfile(@ModelAttribute Profile profile, Model model) {
+		model.addAttribute("profile", profile);
+		try {
+			this.profileDao.save(profile);
+			model.addAttribute("profile", profile);
+			return "form";
+		} catch (Exception e) {
+			throw new ResourceNotFoundException("Profile not found!");
+		}
+	}
+
+	// question 1 and 2
 	@RequestMapping("/profile/{id}")
 	public String getProfile(@PathVariable(value = "id") String id,
 			@PathParam(value = "brief") boolean brief, Model model) {
 		try {
 			Profile profile = profileDao.findById(id);
 			if (profile == null) {
-				throw new ResourceNotFoundException("Profile not found!");
+				throw new ResourceNotFoundException(
+						"Sorry, the requested user with id" + id
+								+ "does not exits!");
 			}
 			model.addAttribute("profile", profile);
 			if (brief) {
@@ -61,30 +74,45 @@ public class ProfileController {
 		}
 	}
 
+	// question 3
 	@RequestMapping(value = "/profile", method = RequestMethod.GET)
-	// @ResponseBody
 	public String create(Model model) {
 		model.addAttribute("profile", new Profile());
 		return "create";
 	}
-	@RequestMapping(value = "/profile", method = RequestMethod.POST)
-	// @ResponseBody
-	public String createSuccess(@ModelAttribute Profile profile,Model model) {
-		model.addAttribute("profile", profile);
-		this.profileDao.save(profile);
-		return "form";
-	}
 
-	@RequestMapping("/delete")
-	@ResponseBody
-	public String delete(String id) {
-		try {
-			Profile profile = new Profile();
-			profile.setId(id);
-			profileDao.delete(profile);
-		} catch (Exception ex) {
-			return "Error deleting the profile:" + ex.toString();
+	// question 3
+	@RequestMapping(value = "/profile", method = RequestMethod.POST)
+	public String createSuccess(@ModelAttribute Profile profile,
+			@RequestParam(value = "action", required = true) String action,
+			Model model) {
+		if (action.equals("delete")) {
+			Profile profile1 = profileDao.findById(profile.getId());
+			if (profile1 == null) {
+				throw new ResourceNotFoundException(
+						"Sorry, the requested user with id" + profile.getId()
+								+ "does not exits!");
+			}
+			this.profileDao.delete(profile1);
+			model.addAttribute("profile", new Profile());
+			return "redirect:/profile";
 		}
-		return "Profile succesfully deleted!";
+		if (action.equals("update")) {
+			Profile profile1 = profileDao.findById(profile.getId());
+			if (profile1 == null) {
+				throw new ResourceNotFoundException(
+						"Sorry, the requested user with id" + profile.getId()
+								+ "does not exits!");
+			}
+			this.profileDao.save(profile);
+			model.addAttribute("profile", profile);
+			return "redirect:/profile/"+ profile.getId();
+		}
+		if (action.equals("create")) {
+			model.addAttribute("profile", profile);
+			this.profileDao.save(profile);
+			return "form";
+		}
+		return action;
 	}
 }
