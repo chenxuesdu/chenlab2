c1073b6a68c093c483275b367bf5249375c2ac0b
chenxuesdu <chenxuesdu@gmail.com>
2016-03-25 19:07:23 -0700

    add delete
    add delete function

--- src/main/java/com/cmpe275/controllers/ProfileController.java
+++ src/main/java/com/cmpe275/controllers/ProfileController.java
@@ -5,13 +5,13 @@ import javax.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.cmpe275.exceptions.ResourceNotFoundException;
 import com.cmpe275.model.Profile;
 import com.cmpe275.model.ProfileDao;
 
@@ -22,63 +22,60 @@ public class ProfileController {
 	ProfileDao profileDao;
 
 	@RequestMapping("/greeting")
-	public String hello(@RequestParam(value = "firstname", required = false, defaultValue = "World") String firstname,
+	public String hello(
+			@RequestParam(value = "firstname", required = false, defaultValue = "World") String firstname,
 			Model model) {
 		model.addAttribute("firstname", firstname);
 		return "hello";
 	}
-	
-	@RequestMapping("/profile/{id}")
-	public String getProfile(@PathVariable(value = "id") String id, @PathParam(value = "brief") boolean brief,
-			Model model) {
+	@RequestMapping("/profile/delete/{id}")
+	public String deleteProfile(@PathVariable(value = "id") String id) {
 		try {
 			Profile profile = profileDao.findById(id);
+			if (profile == null) {
+				throw new ResourceNotFoundException("Profile not found!");
+			}
+			this.profileDao.delete(profile);
+			return "create";
+		} catch (Exception e) {
+			throw new ResourceNotFoundException("Profile not found!");
+		}
+	}
+	
+	@RequestMapping("/profile/{id}")
+	public String getProfile(@PathVariable(value = "id") String id,
+			@PathParam(value = "brief") boolean brief, Model model) {
+		try {
+			Profile profile = profileDao.findById(id);
+			if (profile == null) {
+				throw new ResourceNotFoundException("Profile not found!");
+			}
 			model.addAttribute("profile", profile);
 			if (brief) {
 				return "plain";
 			}
-			return "hello";
-		}catch (Exception e) {
-			return "Error creating the profile";
+			return "form";
+		} catch (Exception e) {
+			throw new ResourceNotFoundException("Profile not found!");
 		}
 	}
 
-	@RequestMapping(value = "/create", method = RequestMethod.POST)
-	//@ResponseBody
-	public String create(@ModelAttribute("profileForm")String id, String firstname, String lastname,Model model) {
+	@RequestMapping(value = "/profile", method = RequestMethod.GET)
+	// @ResponseBody
+	public String create(Model model) {
+		return "create";
+	}
+
+	@RequestMapping("/delete")
+	@ResponseBody
+	public String delete(String id) {
 		try {
 			Profile profile = new Profile();
 			profile.setId(id);
-			profile.setFirstname(firstname);
-			profile.setLastname(lastname);
-			profileDao.save(profile);
-			id = String.valueOf(profile.getId());
-			model.addAttribute("id", profile.getId());
-			model.addAttribute("firstname", profile.getFirstname());
-			model.addAttribute("lastname", profile.getLastname());
-			return "index";
-		} catch (Exception e) {
-			return "Error creating the profile";
+			profileDao.delete(profile);
+		} catch (Exception ex) {
+			return "Error deleting the profile:" + ex.toString();
 		}
+		return "Profile succesfully deleted!";
 	}
-	@RequestMapping(value = "/profile", method = RequestMethod.GET)
-	//@ResponseBody
-	public String createpage(Model model) {
-		Profile profileForm = new Profile();
-		model.addAttribute("profileForm", profileForm);
-		return "profile";
-	}
-	 @RequestMapping("/delete")
-	  @ResponseBody
-	  public String delete(String id) {
-	    try {
-	      Profile profile = new Profile();
-	      profile.setId(id);
-	      profileDao.delete(profile);
-	    }
-	    catch (Exception ex) {
-	      return "Error deleting the profile:" + ex.toString();
-	    }
-	    return "Profile succesfully deleted!";
-	  }
 }
